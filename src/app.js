19acd4d8486c849053eb4a3470b83666a129b129
luciano <lpereira2306@gmail.com>
2022-10-23 08:42:05 -0300

    fixed things

--- src/app.js
+++ src/app.js
@@ -9,11 +9,6 @@ app.use(express.json());
 
 app.use(userRouter);
 
-app.get('/status', (req, res) => {
-	console.log('okay!');
-	res.send('okay2!');
-});
-
 app.listen(process.env.PORT, () => {
 	console.log(`Server is listening on port ` + process.env.PORT);
 });
