f186ce5bc51ce1673ad79506193e781fd09b6369
Rodolfo Sousa <rodolfozsousa@gmail.com>
2022-10-25 22:33:41 -0300

    feat: only show posts by followed users

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -1,21 +1,29 @@
 import connection from "../database/postgres.js";
 
 async function getPostsByHashtagName(name) {
-    return await connection.query(
-        `SELECT posts.*
+  return await connection.query(
+    `SELECT posts.*
         FROM posts
         JOIN "postsHashtags"
         ON posts.id = "postsHashtags"."postId"
         JOIN hashtags
         ON hashtags.id = "postsHashtags"."hashtagId"
         WHERE hashtags.name = $1;`,
-        [name]
-    );
-};
+    [name]
+  );
+}
 
-function selectPosts() {
+function selectPosts(id) {
   return connection.query(
-    `SELECT posts.id, posts."userId", posts.description "postDescription", posts.link "postLink", users.name "userName", users.image "userImage" FROM posts JOIN users ON posts."userId" = users.id ORDER BY posts.id DESC LIMIT 20;`
+    `SELECT posts.id, posts."userId", posts.description "postDescription", posts.link "postLink", users.name "userName", users.image "userImage" FROM follows JOIN posts ON follows."followedId" = posts."userId" JOIN users ON follows."followedId" = users.id WHERE follows."followerId" = $1 ORDER BY posts.id DESC;`,
+    [id]
+  );
+}
+
+function selectPeopleIFollow(id) {
+  return connection.query(
+    `SELECT follows."followedId" FROM follows WHERE follows."followerId" = $1;`,
+    [id]
   );
 }
 
@@ -28,21 +36,31 @@ function insertPost({ userId, description, link }) {
   );
 }
 
-async function deleteThisPost({postId}){
+async function deleteThisPost({ postId }) {
   try {
-  const post = await connection.query('SELECT * FROM posts WHERE id=$1;',[postId])
-  await connection.query('DELETE FROM posts WHERE id=$1;',[postId])
-  return post
+    const post = await connection.query("SELECT * FROM posts WHERE id=$1;", [
+      postId,
+    ]);
+    await connection.query("DELETE FROM posts WHERE id=$1;", [postId]);
+    return post;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-  
 }
 
-async function updateThisPost({postId, description}){
-  await connection.query('UPDATE posts SET description=$1 WHERE id=$2;',[description, postId])
-  return connection.query('SELECT * FROM posts WHERE id=$1;',[postId]);
+async function updateThisPost({ postId, description }) {
+  await connection.query("UPDATE posts SET description=$1 WHERE id=$2;", [
+    description,
+    postId,
+  ]);
+  return connection.query("SELECT * FROM posts WHERE id=$1;", [postId]);
 }
 
-
-export { selectPosts, insertPost, deleteThisPost, updateThisPost, getPostsByHashtagName };
+export {
+  selectPosts,
+  insertPost,
+  deleteThisPost,
+  updateThisPost,
+  getPostsByHashtagName,
+  selectPeopleIFollow,
+};
